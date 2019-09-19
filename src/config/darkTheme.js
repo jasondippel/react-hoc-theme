61948e194c3d0a2c391d4a62d7a76d4e3e0935bc
jasondippel <dippelj12@gmail.com>
2019-09-18 22:41:03 -0700

    dark theme values

---
+++ src/config/darkTheme.js
@@ -0,0 +1,51 @@
+export const VERSION = 'dark.0'
+
+export const defaultTheme = {
+  /**********
+   ** TEXT **
+   **********/
+  text: '#F5FAFC',
+  textInverse: '#2F3638',
+  textMeta: '#aeb4b5',
+  textDisabled: '#81888c',
+  link: '#008cbf',
+
+  /****************
+   ** BACKGROUND **
+   ****************/
+  background100: '#2F3638',
+  background200: '#2D3032',
+  background300: '#222729',
+  background400: '#384042',
+  background500: '#3f4547',
+  background600: '#FFFFFF',
+  keyline: '#191d1f',
+
+  /***********************
+   ** ACCENTS - PRIMARY **
+   ***********************/
+  primary: '#008abd',
+  primaryAccent: '#064760',
+  primaryBackground: '#042e3e',
+
+  /***********************
+   ** ACCENTS - SUCCESS **
+   ***********************/
+  success: '#3b9216',
+  successAccent: '#204f0c',
+  successBackground: '#173a09',
+
+  /***********************
+   ** ACCENTS - WARNING **
+   ***********************/
+  warning: '#c76a00',
+  warningAccent: '#5c3100',
+  warningBackground: '#3d2100',
+
+  /*********************
+   ** ACCENTS - ERROR **
+   *********************/
+  error: '#e00000',
+  errorAccent: '#5c0000',
+  errorBackground: '#3d0000',
+}
--- src/config/defaultTheme.js
+++ src/config/defaultTheme.js
@@ -1,51 +1,101 @@
-export const VERSION = '0'
+export const VERSION = 'light.0'
+
+// export const defaultTheme = {
+//   /**********
+//    ** TEXT **
+//    **********/
+//   text: '#2f3638',
+//   textInverse: '#F5FAFC',
+//   textMeta: '#5D6366',
+//   textDisabled: '#70767a',
+//   link: '#0078A4',
+//
+//   /****************
+//    ** BACKGROUND **
+//    ****************/
+//   background100: '#FFFFFF',
+//   background200: '#FAFAFA',
+//   background300: '#F1F1F1',
+//   background400: '#E3E7E8',
+//   background500: '#D9DDDE',
+//   background600: '#2f3638',
+//   keyline: '#CDD2D4',
+//
+//   /***********************
+//    ** ACCENTS - PRIMARY **
+//    ***********************/
+//   primary: '#00AEEF',
+//   primaryAccent: '#B3E7FB',
+//   primaryBackground: '#DEF4FD',
+//
+//   /***********************
+//    ** ACCENTS - SUCCESS **
+//    ***********************/
+//   success: '#7AAF2C',
+//   successAccent: '#C0DF95',
+//   successBackground: '#EEF7E2',
+//
+//   /***********************
+//    ** ACCENTS - WARNING **
+//    ***********************/
+//   warning: '#F4B21E',
+//   warningAccent: '#FFE380',
+//   warningBackground: '#FFF1C0',
+//
+//   /*********************
+//    ** ACCENTS - ERROR **
+//    *********************/
+//   error: '#E60000',
+//   errorAccent: '#F9BFBF',
+//   errorBackground: '#FBE5E5',
+// }
 
 export const defaultTheme = {
   /**********
    ** TEXT **
    **********/
-  text: '#2f3638',
-  textInverse: '#F5FAFC',
-  textMeta: '#5D6366',
-  textDisabled: '#959A9C',
-  link: '#0078A4',
+  text: '#F5FAFC',
+  textInverse: '#2F3638',
+  textMeta: '#aeb4b5',
+  textDisabled: '#81888c',
+  link: '#008cbf',
 
   /****************
    ** BACKGROUND **
    ****************/
-  background100: '#FFFFFF',
-  background200: '#FAFAFA',
-  background300: '#F1F1F1',
-  background400: '#E3E7E8',
-  background500: '#D9DDDE',
-  background600: '#2f3638',
-  keyline: '#CDD2D4',
+  background100: '#2F3638',
+  background200: '#2D3032',
+  background300: '#222729',
+  background400: '#384042',
+  background500: '#3f4547',
+  background600: '#FFFFFF',
+  keyline: '#191d1f',
 
   /***********************
    ** ACCENTS - PRIMARY **
    ***********************/
-  primary: '#00AEEF',
-  primaryAccent: '#B3E7FB',
-  primaryBackground: '#DEF4FD',
+  primary: '#008abd', // '#00AEEF' // 37%
+  primaryAccent: '#064760', // '#B3E7FB' // 20%
+  primaryBackground: '#042e3e', // '#DEF4FD' // 13%
 
   /***********************
    ** ACCENTS - SUCCESS **
    ***********************/
-  success: '#7AAF2C',
-  successAccent: '#C0DF95',
-  successBackground: '#EEF7E2',
+  success: '#3b9216',
+  successAccent: '#204f0c',
+  successBackground: '#173a09',
 
   /***********************
    ** ACCENTS - WARNING **
    ***********************/
-  warning: '#F4B21E',
-  warningAccent: '#FFE380',
-  warningBackground: '#FFF1C0',
+  warning: '#c76a00',
+  warningAccent: '#5c3100',
+  warningBackground: '#3d2100',
 
   /*********************
    ** ACCENTS - ERROR **
    *********************/
-  error: '#E60000',
-  errorAccent: '#F9BFBF',
-  errorBackground: '#FBE5E5',
+  error: '#e00000',
+  errorAccent: '#5c0000',
+  errorBackground: '#3d0000',
 }
