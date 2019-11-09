accf274bd81f27ac3688d57a885d698d2910d075
jasondippel <dippelj12@gmail.com>
2019-11-09 12:33:56 -0800

    improved color a11y and contrast ratio calculation in example

--- src/config/darkTheme.js
+++ src/config/darkTheme.js
@@ -1,4 +1,4 @@
-export const VERSION = 0
+export const VERSION = 1
 export const TYPE = 'dark'
 
 export const VALUES = {
@@ -8,8 +8,8 @@ export const VALUES = {
   text: '#F5FAFC',
   textInverse: '#2F3638',
   textMeta: '#aeb4b5',
-  textDisabled: '#81888c',
-  link: '#008cbf',
+  textDisabled: '#999fa3',
+  link: '#00aceb',
 
   /****************
    ** BACKGROUND **
@@ -19,34 +19,36 @@ export const VALUES = {
   background300: '#222729',
   background400: '#384042',
   background500: '#3f4547',
-  background600: '#FFFFFF',
+  background600: '#FFFFFF', // NOTE: this is deprecated; will be removed in near future
+  backgroundLight: '#FFFFFF',
+  backgroundDark: '#1c2021',
   keyline: '#191d1f',
 
   /***********************
    ** ACCENTS - PRIMARY **
    ***********************/
-  primary: '#008abd',
-  primaryAccent: '#064760',
+  primary: '#007ba8',
+  primaryAccent: '#074a64',
   primaryBackground: '#042e3e',
 
   /***********************
    ** ACCENTS - SUCCESS **
    ***********************/
-  success: '#3b9216',
+  success: '#348113',
   successAccent: '#204f0c',
   successBackground: '#173a09',
 
   /***********************
    ** ACCENTS - WARNING **
    ***********************/
-  warning: '#c76a00',
-  warningAccent: '#5c3100',
-  warningBackground: '#3d2100',
+  warning: '#cc4100',
+  warningAccent: '#7a2700',
+  warningBackground: '#5c1d00',
 
   /*********************
    ** ACCENTS - ERROR **
    *********************/
-  error: '#e00000',
-  errorAccent: '#5c0000',
-  errorBackground: '#3d0000',
+  error: '#c20000',
+  errorAccent: '#800000',
+  errorBackground: '#5c0000',
 }
